6c287ef0628c62434b0688c89a48aaabbff19705
Zhu-Peter <gzhu@ualberta.ca>
2024-04-30 19:59:25 -0600

    mis changes

--- src/components/VueBody.vue
+++ src/components/VueBody.vue
@@ -1,22 +1,39 @@
 <template>
-    <div class="VueBody">
-      
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'VueBody',
-    props: {
-      msg: String
-    }
+  <div class="VueBody">
+    <h1>Title</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste omnis quia dolores quam eaque et enim inventore
+      repudiandae perspiciatis? Blanditiis, consectetur facilis. Cum voluptatem quam ratione et excepturi velit tempora
+      veniam temporibus magni dolorum. Debitis minus fugit dignissimos est. Quo veritatis temporibus, blanditiis maiores
+      ut reprehenderit tenetur. Dicta aspernatur officiis obcaecati eveniet perferendis beatae quibusdam ex a aliquid
+      tenetur, sequi consequuntur eius, excepturi omnis consectetur assumenda culpa laudantium sit ea iusto animi.
+      Pariatur accusantium omnis id ullam odit accusamus voluptatem, eum unde deserunt neque ut sequi possimus quo
+      sapiente cupiditate facere perferendis repudiandae necessitatibus fuga nesciunt itaque a asperiores temporibus?
+      Perferendis non, saepe vel accusamus sunt nihil consequatur dolore totam? Molestiae cupiditate, ullam veniam
+      tempora ex eum quidem veritatis in minima consectetur sapiente delectus, quod, culpa dolores? Unde odit enim
+      officiis illo accusantium eius iure adipisci repellat harum nostrum voluptates velit natus dolore, cum laudantium
+      repudiandae voluptas a qui vel impedit quam sunt vero porro esse. Vero facilis aliquam eius exercitationem,
+      voluptatibus distinctio ab nemo, vitae dolores ad magnam corrupti eaque. Laboriosam ipsum tempora ratione neque
+      ipsa sapiente quidem excepturi. Quod eius vero reiciendis nam. Voluptate eligendi libero mollitia esse eaque, enim
+      modi quo itaque illo dicta rerum, pariatur impedit!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueBody',
+  props: {
+    msg: String
   }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-    .VueBody{
-      background-color: aquamarine;
-    }
-  </style>
-  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.VueBody {
+  box-sizing: border-box;
+  background-color: aquamarine;
+  width: 1400px;
+  margin-inline: auto;
+  padding: 50px;
+}
+</style>
--- src/components/VueHeader.vue
+++ src/components/VueHeader.vue
@@ -3,6 +3,7 @@
     <div class="page_header_layout">
       <div class="page_header_logo">
         <img id="page_header_logo" src="../assets/logo.png" alt="logo" />
+        <h1 id="page_header_title">My Vue Page</h1>
       </div>
       <nav class="page_header_menu">
         <svg class="page_header_menu_icon" version="1.1" id="menu_icon" viewBox="0 0 16 16" x="0px" y="0px"
@@ -20,17 +21,23 @@
 
 <script>
 export default {
-  name: 'VueHeader',
+  name: "VueHeader",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.page_header {
+* {
+  box-sizing: border-box;
+}
+
+.page_header_layout {
   display: block;
+  background-color: lightblue;
+  padding-inline: 50px;
 }
 
 .page_header_layout {
@@ -38,13 +45,19 @@ export default {
   flex-direction: column;
   align-items: start;
   min-height: 80px;
-  width: 1200px;
+  width: 1400px;
   margin: 0 auto;
+  margin-bottom: 60px;
 }
 
 #page_header_logo {
   width: 100px;
   margin: 22px auto 0 0;
+  display: inline;
+}
+
+#page_header_title {
+  display: inline;
 }
 
 nav {
@@ -53,12 +66,13 @@ nav {
 }
 
 #menu_icon {
-  fill: orange;
+  fill: darkblue;
   height: 2em;
   min-width: auto;
+  margin-right: 50px;
 }
 
-#menu_icon:hover{
+#menu_icon:hover {
   cursor: pointer;
 }
 </style>
